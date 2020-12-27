c646a43d996b2c74590e0de64bd0161c0ed237bd
xuexue <1157354643@qq.com>
2020-12-27 13:01:18 +0900

    test

---
+++ hamster/src/test/java/com/example/hamster/HamsterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hamster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HamsterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
