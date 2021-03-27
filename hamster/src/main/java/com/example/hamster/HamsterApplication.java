f69fdaa1bb7664042248a6ffb559de8de8113568
chengxu.han <chengxuhan123@126.com>
2021-03-27 18:24:35 +0900

    外部tomcatに変更

--- hamster/src/main/java/com/example/hamster/HamsterApplication.java
+++ hamster/src/main/java/com/example/hamster/HamsterApplication.java
@@ -2,12 +2,19 @@ package com.example.hamster;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class HamsterApplication {
+public class HamsterApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(HamsterApplication.class, args);
 	}
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application){
+        //找到原先的启动类
+        return application.sources(HamsterApplication.class);
+    }
 }
