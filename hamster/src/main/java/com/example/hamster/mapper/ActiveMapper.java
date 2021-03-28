f84add2ff70464cb7c828439c41fd25fd4371a3f
xuexue <1157354643@qq.com>
2021-03-28 16:08:17 +0900

    lapCount取得

--- hamster/src/main/java/com/example/hamster/mapper/ActiveMapper.java
+++ hamster/src/main/java/com/example/hamster/mapper/ActiveMapper.java
@@ -1,8 +1,12 @@
 package com.example.hamster.mapper;
 
+import java.util.Date;
+import java.util.List;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Options;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
 import com.example.hamster.entity.Active;
 
@@ -13,4 +17,7 @@ public interface ActiveMapper {
             + "values(#{hamster_id},#{lap_count},#{insert_date_time},#{insert_user})")
     @Options(useGeneratedKeys = true, keyColumn = "active_id", keyProperty = "active_id")
      public void insertOne(Active active);
+
+	 @Select("select * from active where insert_date_time BETWEEN #{startTime} AND #{endTime}  and hamster_id =#{hamsterId} order by insert_date_time desc")
+	public List<Active> selectLapCountByDate(@Param("hamsterId") int hamsterId, @Param("startTime") Date startTime, @Param("endTime") Date endTime);
 }
--- hamster/src/main/java/com/example/hamster/service/ActiveService.java
+++ hamster/src/main/java/com/example/hamster/service/ActiveService.java
@@ -1,5 +1,10 @@
 package com.example.hamster.service;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,4 +19,22 @@ public class ActiveService {
     public void insertOne(Active active) {
         activeMapper.insertOne(active);
     }
+    public Integer findLapCount(int hamsterId, String startDate, String endDate) {
+    	SimpleDateFormat dateformat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+		Date startTime= null;
+		Date endTime = null;
+		try {
+			startTime = dateformat.parse(startDate);
+			endTime	= dateformat.parse(endDate);
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+
+    	List<Active> activeList = activeMapper.selectLapCountByDate(hamsterId, startTime, endTime);
+    	int maxCount = activeList.get(0).getLap_count();
+    	int mixCount = activeList.get(activeList.size()-1).getLap_count();
+    	int lapCount = maxCount - mixCount;
+		return lapCount;
+    }
+
 }
