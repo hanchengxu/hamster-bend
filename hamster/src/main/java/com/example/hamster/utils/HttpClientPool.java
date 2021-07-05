944271e1bb667369b026d3c768f67388e25e7595
chengxu.han <chengxuhan123@126.com>
2021-07-05 23:30:37 +0900

    add httpclient

---
+++ hamster/src/main/java/com/example/hamster/utils/HttpClientPool.java
@@ -0,0 +1,31 @@
+package com.example.hamster.utils;
+
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+
+public class HttpClientPool {
+
+    private CloseableHttpClient httpclient;
+
+
+    private static class HttpClientPoolInstance{
+        private static final HttpClientPool instance = new HttpClientPool();
+    }
+
+    private HttpClientPool() {
+        /**
+         * Assigns maximum total connection value.
+         * default: 30
+         */
+        httpclient = HttpClients.custom().setMaxConnTotal(30).build();
+    }
+
+    public static HttpClientPool getInstance() {
+        return HttpClientPoolInstance.instance;
+    }
+
+    public CloseableHttpClient getHttpClient() {
+        return this.httpclient;
+    }
+
+}
